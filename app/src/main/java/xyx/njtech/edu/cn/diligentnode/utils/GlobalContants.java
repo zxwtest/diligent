0388d9ae80d4f001977edad43408210c98c9d97f
zxwtest <xiangweizhang@163.com>
2018-04-21 22:23:24 +0800

    修改图片和logo

--- app/src/main/java/xyx/njtech/edu/cn/diligentnode/utils/GlobalContants.java
+++ app/src/main/java/xyx/njtech/edu/cn/diligentnode/utils/GlobalContants.java
@@ -2,5 +2,5 @@ package xyx.njtech.edu.cn.diligentnode.utils;
 
 public class GlobalContants {
 
-    public static final String SERVER_URL="";
+    public static final String SERVER_URL="";//"http://10.32.1.88/xyx";
 }
